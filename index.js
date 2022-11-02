cb7ece460e4571735edcfc28c6d71c6e4c92d8c9
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-11-02 17:30:23 -0500

    commit tree 6fa5aa559fe0c987ad5fd3b60dc2edfa329e390e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const Hq = 42;
+  return Math.abs(someValue - Hq);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs(block1 - block2) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  let price;
+  if (distance <= 400) {
+    price = 0;
+  } else if (400 < distance && distance <= 2000) {
+    price = (distance - 400) * 0.02;
+  } else if (2000 < distance && distance < 2500) {
+    price = 25;
+  } else if (distance > 2500) {
+    return "cannot travel that far";
+  }
+
+  return price;
+}
